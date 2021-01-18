fbe8570f686f0f983faa94e92781f11941ca3ddf
Oyesh <oyeshsin@hotmail.com>
2021-01-18 15:57:08 -0500

    Modified to disable cache

--- flaskAPI/app.py
+++ flaskAPI/app.py
@@ -4,12 +4,14 @@ from urllib.request import urlopen
 from PIL import Image
 import numpy as np
 import cv2
+import requests
 from resources.processimage import Base64Endpoint, MultiPartEndpoint, ImageUrlEndpoint
 import logging
 
 
 app = Flask(__name__)
 api = Api(app)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 api.add_resource(ImageUrlEndpoint, '/magic/imageurl')
 api.add_resource(Base64Endpoint, '/magic/base64')
@@ -22,23 +24,30 @@ api.add_resource(MultiPartEndpoint, '/magic/multipart')
 # Just to confirm visually
 @app.route('/', methods=['GET'])
 def home():
-    image_url = 'https://images.dog.ceo/breeds/leonberg/n02111129_2088.jpg'
-    save_file_path = "static/images/bw.png"
+    with requests.request("GET", url="https://dog.ceo/api/breeds/image/random") as r:
+        if r.status_code == 200:
+            # To handle the response object sent using flask jsonify
+            response = r.json()
+            print(response["message"])
 
-    # Transformation using Pillow
-    # transformer = ProcessImageEndpoint()
-    # transformed_image = transformer.transform_image(image_url)
-    # transformed_image.save(save_file_path, "PNG")
-    # return render_template('index.html', image_url = [image_url, save_file_path])
+            # image_url = 'https://images.dog.ceo/breeds/leonberg/n02111129_2088.jpg'
+            image_url = response["message"]
+            save_file_path = "./static/images/bw.png"
 
-    # Transformation using OpenCV
-    url_response = urlopen(image_url)
-    img_array = np.array(bytearray(url_response.read()), dtype=np.uint8)
-    originalImage = cv2.imdecode(img_array, -1)
-    grayImage = cv2.cvtColor(originalImage, cv2.COLOR_BGR2GRAY)
-    (thresh, blackAndWhiteImage) = cv2.threshold(grayImage, 127, 255, cv2.THRESH_BINARY)
-    cv2.imwrite(save_file_path, blackAndWhiteImage)
-    return render_template('index.html', image_url=[image_url, save_file_path])
+            # Transformation using Pillow
+            # transformer = ProcessImageEndpoint()
+            # transformed_image = transformer.transform_image(image_url)
+            # transformed_image.save(save_file_path, "PNG")
+            # return render_template('index.html', image_url = [image_url, save_file_path])
+
+            # Transformation using OpenCV
+            url_response = urlopen(image_url)
+            img_array = np.array(bytearray(url_response.read()), dtype=np.uint8)
+            originalImage = cv2.imdecode(img_array, -1)
+            grayImage = cv2.cvtColor(originalImage, cv2.COLOR_BGR2GRAY)
+            (thresh, blackAndWhiteImage) = cv2.threshold(grayImage, 127, 255, cv2.THRESH_BINARY)
+            cv2.imwrite(save_file_path, blackAndWhiteImage)
+            return render_template('index.html', image_url=[image_url, save_file_path])
 
 
 app.run()
